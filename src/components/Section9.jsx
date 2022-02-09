7e4c04ff7175430c92605f5d9574d634d81eee0d
Wojciech Fydrych <wfydrych@joinpiggy.com>
2022-02-09 17:44:03 +0100

    Added configuration section

--- src/components/Section9.jsx
+++ src/components/Section9.jsx
@@ -13,6 +13,133 @@ const Section9 = () => {
         Sie sind sich nicht sicher, wie Sie das machen können? Konfigurieren Sie Ihre Treppe und passen Sie sie mit unserem Spezialisten an Ihr Interieur an. 
         Kontaktieren Sie ihn telefonisch oder über das Online-Formular am Ende der Seite und erkundigen Sie sich nach den Preisen für Stahltreppen.
       </div>
+
+      <div className='section9__config'>MODULARE TREPPEN – KONFIGURIEREN</div>
+
+      <div className='section9__title'>1. WÄHLEN SIE EINE TREPPE FÜR SICH</div>
+      <div className='section9__config__container'>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2019/05/modultreppe-27.jpg' alt='treppe' />
+          <span>Gitterroste</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2021/11/modultreppe-asta.jpg' alt='treppe' />
+          <span>Profilroste</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2019/05/modultreppe-30.jpg' alt='treppe' />
+          <span>Bretter für den Außenbereich rutschfest (Lärche)</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2019/05/modultreppe-26.jpg' alt='treppe' />
+          <span>Bretter für den Innenbereich verleimt (Buche)</span>
+        </div>
+      </div>
+
+      <div className='section9__title'>2. PODEST AUSWÄHLEN</div>
+      <div className='section9__config__info'>Podeste und Wendelpodeste sind in 14 Konfigurationen verfügbar</div>
+      <div className='section9__config__container'>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-1-1.jpg' alt='treppe' />
+          <span>Podest auf Stützen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-2-1.jpg' alt='treppe' />
+          <span>Podest auf Stützen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-3-1.jpg' alt='treppe' />
+          <span>Podest auf Stützen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-4-1.jpg' alt='treppe' />
+          <span>Podest auf Stützen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-5.jpg' alt='treppe' />
+          <span>Podest auf Säulen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-6.jpg' alt='treppe' />
+          <span>Podest auf Säulen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-7.jpg' alt='treppe' />
+          <span>Podest auf Säulen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-8.jpg' alt='treppe' />
+          <span>Podest auf Säulen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-9.jpg' alt='treppe' />
+          <span>Podest auf Säulen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-10.jpg' alt='treppe' />
+          <span>Podest auf Säulen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-11.jpg' alt='treppe' />
+          <span>Podest auf Säulen</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-12.jpg' alt='treppe' />
+          <span>Wendelpodeste</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-13.jpg' alt='treppe' />
+          <span>Wendelpodeste</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/modultreppe-14.jpg' alt='treppe' />
+          <span>Doppelpodest</span>
+        </div>
+      </div>
+
+      <div className='section9__title'>3. ABSPERRUNG AUSWÄHLEN</div>
+      <div className='section9__config__container'>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/Modultreppe-2.jpg' alt='treppe' />
+          <span>Absperrung ohne Füllung</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/Modultreppe-3.jpg' alt='treppe' />
+          <span>Absperrung ohne Füllung</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/Modultreppe-4.jpg' alt='treppe' />
+          <span>Seile fi 4 mm</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2020/06/Modultreppe-1.jpg' alt='treppe' />
+          <span>Rohr fi 12 mm</span>
+        </div>
+        <div className='section9__config__container__item'>
+          <img src='https://www.intertlc.de/wp-content/uploads/2021/10/ABSPERRUNG.jpg' alt='treppe' />
+          <span>Sichere Barriere</span>
+        </div>
+      </div>
+
+      <div className='section9__title'>4. ANZAHL VON MODULEN AUSWÄHLEN</div>
+      <div className='section9__config__fotocontainer'>
+        <img className='section9__config__foto' src='https://www.intertlc.de/wp-content/uploads/2019/05/modultreppe-25.jpg' alt='treppe-modulen-auswahlen' />
+      </div>
+
+      <div className='section9__bg'>
+        <div className='section9__bg__btn'>
+          <div className='section9__bg__btn__columnleft'>
+            Benötigen Sie Hilfe beim Einrichten <br />Ihrer Treppe? <br />
+            Kontaktieren Sie unseren Spezialisten!
+          </div>
+          <div className='section9__bg__btn__columnright'>
+            Sebastian Wirmanski<br />
+            +41 791 746 555<br />
+            kronenkar@gmx.ch
+          </div>
+        </div>
+      </div>
+      
     </div>
   )
 }
